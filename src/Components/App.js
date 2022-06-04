7ae1ec8489b76040865191dac7dcf06afe603ca7
bub <bub@>
2022-06-03 23:44:55 -0500

    initialize

---
+++ src/Components/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+    min-width: 100vw;
+    min-height: 100vh;
+`;
+
+const App = () => {
+
+    return (
+        <AppWrapper>
+            <h1>yo</h1>
+            <p>im app</p>
+        </AppWrapper>
+    );
+}
+
+export default App;
